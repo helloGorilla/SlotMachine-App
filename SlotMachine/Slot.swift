7845bc78c5f4e52f6c06d2f76005317fd62ab375
Bryan Stedman <bryanbstedman@gmail.com>
2014-11-05 20:44:51 -0700

    Add changes through 'SlotMachine: Showing Slots (03:15)'

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,17 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by Bryan Stedman on 11/3/14.
+//  Copyright (c) 2014 Bryan Stedman. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+    
+}
